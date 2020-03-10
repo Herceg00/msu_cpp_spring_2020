fbdfa0e79687443d2a46e37bb644f6bb44787a44
Dmitry <dimlichmanov@gmail.com>
2020-03-11 00:16:26 +0300

    Add files via upload

--- 1/allocator.h
+++ 1/allocator.h
@@ -1,40 +1,36 @@
 #pragma once
+
 #include <iostream>
 
-class Allocator{
-    char* pointer;
+class Allocator {
+    char *pointer;
     size_t size;
     size_t offset;
 public:
-    explicit Allocator(size_t _size):pointer(nullptr),size(0),offset(0) {
-        if(_size > 0) {
-            pointer = (char*)malloc(_size);
-            if (pointer != nullptr) {
-                size = _size;
-            }
+
+    explicit Allocator(size_t _size) : pointer(nullptr), size(0), offset(0) {
+        pointer = (char *) malloc(_size);
+        if (pointer != nullptr) {
+            size = _size;
         }
-    }
-    char* alloc(size_t _size) {
-        if ((int)_size <= 0) {
-            printf("0 bytes allocated\n");
-            return pointer;
-        } else {
-            if (offset + _size <= size) {
-                char *temp_pointer = pointer + offset;
-                offset += _size;
-                printf("Allocated %d bytes, total alloc usage is %d bytes\n", (int) _size, (int) offset);
-                return temp_pointer;
-            }
-            printf("Too much memory required, only %d bytes is available\n", (int) size - (int) offset);
-            return nullptr;
-        }
-    }
-    void reset(void){
-        offset  = 0;
-        printf("Reset completed, %d bytes is available\n", (int)(size-offset));
+
     }
 
-    ~Allocator(){
+    char *alloc(size_t _size) {
+        if (offset + _size <= size) {
+            char *temp_pointer = pointer + offset;
+            offset += _size;
+            return temp_pointer;
+        }
+        return nullptr;
+
+    }
+
+    void reset(void) {
+        offset = 0;
+    }
+
+    ~Allocator() {
         free(pointer);
     }
 };
