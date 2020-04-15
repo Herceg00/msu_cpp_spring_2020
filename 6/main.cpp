5b06b46b213319e8014d3f5b3a758c69a8d1db4d
Dmitry <dimlichmanov@gmail.com>
2020-04-15 12:26:39 +0300

    Add files via upload

--- 6/main.cpp
+++ 6/main.cpp
@@ -2,40 +2,58 @@
 #include "string"
 #include "vector"
 #include "assert.h"
+#include "sstream"
 
 class String_Handler {
+
     int num_of_args = 0;
     std::string data;
     std::vector<std::string> args_array;
 
 public:
-    String_Handler(const char *_data) {
+    explicit String_Handler(const char *_data) {
         data = _data;
+    }
+
+    template<typename T>
+    void get_args(T &&arg) {
+        num_of_args++;
+        std::stringstream temp;
+        temp<<arg;
+        args_array.push_back(temp.str());
     }
 
     template<typename T, typename... Args>
     void get_args(T &&arg, Args &&...args) {
-        if (sizeof...(args) > num_of_args) {
-            num_of_args = sizeof...(args);
-        }
-        args_array.push_back(arg);
+        num_of_args++;
+        std::stringstream temp;
+        temp<<arg;
+        args_array.push_back(temp.str());
         get_args(std::forward<Args>(args)...);
     }
 
     std::string process() {
         std::string formatted;
         int num_counter = 0;
-        std::cout<<data<<std::endl;
-        std::cout<<num_of_args<<std::endl;
+        std::cout<<"DATA:"<<data<<std::endl;
+        std::cout<<"NUM OF ARGS:"<<num_of_args<<std::endl;
+
+        for (int j = 0; j <num_of_args ; ++j) {
+            std::cout<<args_array[j]<<std::endl;
+        }
+
         for (int i = 0; i < data.size(); i++) {
+            std::cout<<i<<"-th symbol is "<<data[i]<<std::endl;
             if (data[i] == '{') {
+                std::cout<<i<<"-th symbol is "<<data[i]<<std::endl;
                 int num = 0;
                 i++;
                 do {
                     if ((!isdigit(data[i]))) {
                         throw std::runtime_error("wrong format");
                     }
-                    num = 10 * num + data[i];
+                    num = 10 * num + data[i] - '0';
+                    std::cout<<i<<"-th symbol is "<<data[i]<<std::endl;
                     std::cout<<num<<std::endl;
                     i++;
                 } while (data[i] != '}');
@@ -58,9 +76,9 @@ public:
 template <typename ...Args>
 std::string format(const char *str,Args&&...args)
 {
-    String_Handler f(str);
-    f.get_args(std::forward<Args>(args)...);
-    return f.process();
+    String_Handler formatter(str);
+    formatter.get_args(std::forward<Args>(args)...);
+    return formatter.process();
 }
 
 int main(void){
